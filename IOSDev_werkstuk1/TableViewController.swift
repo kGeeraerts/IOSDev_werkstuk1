2117a97b31b516dcb3fb573017fecceaa2b07dc0
kGeeraerts <kwinten.geeraerts@student.ehb.be>
2021-08-01 08:58:20 -0700

    Start tableView functionality

--- IOSDev_werkstuk1/TableViewController.swift
+++ IOSDev_werkstuk1/TableViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class TableViewController: UITableViewController {
+    
+    var Centra = ["Vaccinatiecentrum Overijse", "Vaccinatiecentrum Tervuren"]
+    
+    var desc = ["Vaccinatiecentrum van Overijse", "Vaccinatiecentrum van Tervuren"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,23 +29,26 @@ class TableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return Centra.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "VacCent", for: indexPath)
 
-        // Configure the cell...
+        // Configure the cell
+        
+        cell.textLabel?.text = self.Centra[indexPath.row]
+        cell.detailTextLabel?.text = self.desc[indexPath.row]
 
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
