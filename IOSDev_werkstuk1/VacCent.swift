fb5cb8ed6714172113a2d1bdb229977401f54a41
kGeeraerts <kwinten.geeraerts@student.ehb.be>
2021-08-02 05:08:31 -0700

    Add VacCent class

---
+++ IOSDev_werkstuk1/VacCent.swift
@@ -0,0 +1,31 @@
+//
+//  VacCent.swift
+//  IOSDev_werkstuk1
+//
+//  Created by Quceka on 02/08/2021.
+//  Copyright © 2021 Quceka. All rights reserved.
+//
+
+import UIKit
+
+class VacCent: NSObject {
+    var naam: String
+    var straat: String
+    var huisnummer: Int
+    var postcode: Int
+    var gemeente: String
+    var lat: Double
+    var long: Double
+    var telefoonnummer: String //String in case people add "+" to number
+    
+    init(naam: String, straat: String, huisnummer: Int, postcode: Int, gemeente: String, lat: Double, long: Double, telefoonnummer: String){
+        self.naam = naam
+        self.straat = straat
+        self.huisnummer = huisnummer
+        self.postcode = postcode
+        self.gemeente = gemeente
+        self.lat = lat
+        self.long = long
+        self.telefoonnummer = telefoonnummer
+    }
+}
